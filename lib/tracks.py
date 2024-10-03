0faeb5ecc048366a733b5c63e4762a8ab7a8204f
Yondon Fu <yondon.fu@gmail.com>
2024-10-03 21:19:18 +0000

    Add option to drop frames

--- lib/tracks.py
+++ lib/tracks.py
@@ -15,6 +15,7 @@ class VideoStreamTrack(MediaStreamTrack):
         self.pipeline = pipeline
         self.warmup_frame_idx = 0
         self.warmup_frames = os.getenv("WARMUP_FRAMES", 10)
+        self.drop_frames = int(os.getenv("DROP_FRAMES", 1))
 
     async def recv(self):
         while self.warmup_frame_idx < self.warmup_frames:
@@ -23,9 +24,15 @@ class VideoStreamTrack(MediaStreamTrack):
             self.pipeline(frame)
             self.warmup_frame_idx += 1
 
-        # The decoded frame returned by aiortc will be a torch.Tensor which
-        # is then passed into the pipeline.
-        # The pipeline returns the processed frame as a torch.Tensor which
-        # is then passed to aiortc to be encoded into a frame.
+        # Drop frame which might help with playback in certain scenarios.
+        # Dropping every other frame seems to address stuttering playback when receiving streams from OBS's x264 encoder
+        # even though there is no playback issue when receiving streams from Chrome VideoToolBox without dropping frames.
+        for _ in range(self.drop_frames):
+            await self.track.recv()
+
         frame = await self.track.recv()
+        # The decoded frame returned by aiortc will be a torch.Tensor if NVDEC is used and
+        # an av.VideoFrame if a SW decoder is used.
+        # The pipeline returns the processed frame as a torch.Tensor if NVENC is used and
+        # an av.VideoFrame if a SW encoder is used.which
         return self.pipeline(frame)
